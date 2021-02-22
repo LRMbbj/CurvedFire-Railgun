916916377945c7f4f6e8e12a564928de4e677666
LRMbbj <1325712737@qq.com>
2021-02-22 22:40:12 +0800

    修正了OpenMV的代码

--- OVCamera/main.py
+++ OVCamera/main.py
@@ -7,13 +7,18 @@ uart = UART(3,19200)
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.VGA)
+roi = (170,190,100,100)
+sensor.set_windowing(roi)
 sensor.skip_frames(time = 2000)
 sensor.set_auto_gain(False) # must be turned off for color tracking
 sensor.set_auto_whitebal(False) # must be turned off for color tracking
+sensor.set_brightness(2)
 clock = time.clock()
 
 
 def find_maxc(cs):
+    if len(cs)==0:
+        return None
     max = cs[0]
     if len(cs)==1:
         return max
@@ -25,24 +30,26 @@ def find_maxc(cs):
 
 while(True):
     clock.tick()
-    img = sensor.snapshot().lens_corr(1.8)
+    #img = sensor.snapshot().lens_corr(1.8)
+    img = sensor.snapshot()
     red_circles = []
     for c in img.find_circles(threshold = 3500, x_margin = 10, y_margin = 10, r_margin = 10,
             r_min = 2, r_max = 100, r_step = 2):
         area = (c.x()-c.r(), c.y()-c.r(), 2*c.r(), 2*c.r())
         #area为识别到的圆的区域，即圆的外接矩形框
         statistics = img.get_statistics(roi=area)#像素颜色统计
-        print(statistics)
+        #print(statistics)
         #(0,100,0,120,0,120)是红色的阈值，所以当区域内的众数（也就是最多的颜色），范围在这个阈值内，就说明是红色的圆。
         #l_mode()，a_mode()，b_mode()是L通道，A通道，B通道的众数。
         if 0<statistics.l_mode()<100 and 0<statistics.a_mode()<127 and 0<statistics.b_mode()<127:#if the circle is red
-            img.draw_circle(c.x(), c.y(), c.r(), color = (255, 0, 0))#识别到的红色圆形用红色的圆框出来
+            img.draw_circle(c.x(), c.y(), c.r(), color = (0, 0, 255))#识别到的红色圆形用红色的圆框出来
             red_circles.append(c)
         else:
             img.draw_rectangle(area, color = (255, 255, 255))
             #将非红色的圆用白色的矩形框出来
     max_circle = None
     max_circle = find_maxc(red_circles)
-    if 300<max_circle.x()<340 and 220<max_circle.y()<260:
-        uart.write( "CT\r\n" )
+    if max_circle!=None:
+    uart.write( "CT\r\n" )
         print("In Center")
+    print("FPS %f" % clock.fps())
